aa8c721fb40d6d87995d00e8a758ee2d1690e25c
dinizdv <dinizdev3@gmail.com>
2024-12-11 09:28:01 -0300

    feat(models, controllers): models and controllers creation.

--- API-RESTful/src/router.ts
+++ API-RESTful/src/router.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response } from 'express'
+import { createMovie } from './controller/movieController'
 
 const router = Router()
 
-export default router.get("/test", (req: Request, res: Response) => {
+export default router
+.get("/test", (req: Request, res: Response) => {
     res.status(200).send('API Working!')
+})
+.post('/movie', (req: Request, res: Response) => {
+    createMovie(req, res)
 })
